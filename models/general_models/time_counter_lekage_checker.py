a018c754afc08564c6f3c96cbc5304714b5b0563
snpixel <namdeoshreyas@gmal.com>
2024-07-28 07:32:22 +0530

    added ppts and modified some .py files

--- models/general_models/time_counter_lekage_checker.py
+++ models/general_models/time_counter_lekage_checker.py
@@ -18,7 +18,7 @@ colonies = {
 
 def read_data(colony_name):
     try:
-        return pd.read_csv(f'dataofgrpahs//leak_colony//data_{colony_name}.csv')
+        return pd.read_csv(f'water_managment//dataofgrpahs//leak_colony//data_{colony_name}.csv')
     except FileNotFoundError:
         print(f"File data_{colony_name}.csv not found.")
         return pd.DataFrame(columns=['timestamp', 'tank_level'])
